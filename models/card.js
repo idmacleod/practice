52daed88e233dec843b72241db365a54af8d0f0e
idmacleod <idmacleod@gmail.com>
2020-07-28 16:42:56 +0100

    Added getValue() method to card model

--- models/card.js
+++ models/card.js
@@ -1,6 +1,13 @@
-const Card = function (face, suit) {
-    this.face = face;
+const Card = function (rank, suit) {
+    this.rank = rank;
     this.suit = suit;
+}
+
+Card.prototype.getValue = function () {
+    const values = {'ace': 11, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+                    'six': 6, 'seven' :7, 'eight': 8, 'nine': 9, 'ten': 10,
+                    'jack': 10, 'queen': 10, 'king': 10};
+    return values[this.rank.toLowerCase()];
 }
 
 module.exports = Card;
--- specs/card_spec.js
+++ specs/card_spec.js
@@ -4,17 +4,31 @@ const Card = require('../models/card');
 describe('Card', function () {
     let card1;
     let card2;
+    let card3;
 
     beforeEach(function () {
-        card1 = new Card('Ten', 'Diamonds');
-        card2 = new Card('Queen', 'Hearts');
+        card1 = new Card('ten', 'diamonds');
+        card2 = new Card('queen', 'hearts');
+        card3 = new Card('ace', 'spades');
     });
 
-    it('should have a face', function () {
-        assert.strictEqual(card1.face, 'Ten');
+    it('should have a rank', function () {
+        assert.strictEqual(card1.rank, 'ten');
     });
 
     it('should have a suit', function () {
-        assert.strictEqual(card1.suit, 'Diamonds');
+        assert.strictEqual(card1.suit, 'diamonds');
+    });
+
+    it('should have a value if pip card', function () {
+        assert.strictEqual(card1.getValue(), 10);
+    });
+
+    it('should have a value if face card', function () {
+        assert.strictEqual(card2.getValue(), 10);
+    });
+
+    it('should have a value if aces high', function () {
+        assert.strictEqual(card3.getValue(), 11);
     });
 });
