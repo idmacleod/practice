083295381527b1111992305d6542c5b019101a09
idmacleod <idmacleod@gmail.com>
2020-07-28 16:06:16 +0100

    Wrote test to check that player's hand starts empty

--- specs/player_spec.js
+++ specs/player_spec.js
@@ -1,10 +1,18 @@
-Player = require('../src/player');
-assert = require('assert');
-
-const player1 = new Player('Iain');
+const assert = require('assert');
+const Player = require('../src/player');
 
 describe('Player', function () {
+    let player1;
+
+    beforeEach(function () {
+        player1 = new Player('Iain');
+    });
+
     it('should have a name', function () {
-        assert.strictEqual('Iain', player1.name);
+        assert.strictEqual(player1.name, 'Iain');
+    });
+
+    it('should start with an empty hand', function () {
+        assert.strictEqual(player1.hand.length, 0);
     });
 });
