9374e2a5905680fa2a3f818371f2ae56156a799e
idmacleod <idmacleod@gmail.com>
2020-07-29 09:30:38 +0100

    Adding valueHand() method to Player

--- models/player.js
+++ models/player.js
@@ -7,4 +7,8 @@ Player.prototype.pickUp = function (card) {
     this.hand.push(card);
 }
 
+Player.prototype.valueHand = function () {
+    return this.hand.reduce((acc, card) => acc + card.getValue(), 0);
+}
+
 module.exports = Player;
--- specs/player_spec.js
+++ specs/player_spec.js
@@ -5,10 +5,12 @@ const Card = require('../models/card');
 describe('Player', function () {
     let player1;
     let card1;
+    let card2;
 
     beforeEach(function () {
         player1 = new Player('Iain');
-        card1 = new Card('ace', 'spades');
+        card1 = new Card('jack', 'clubs');
+        card2 = new Card('three', 'diamonds');
     });
 
     it('should have a name', function () {
@@ -22,5 +24,11 @@ describe('Player', function () {
     it('can pick up card', function () {
         player1.pickUp(card1);
         assert.strictEqual(player1.hand.length, 1);
+    });
+
+    it('can value hand', function () {
+        player1.pickUp(card1);
+        player1.pickUp(card2);
+        assert.strictEqual(player1.valueHand(), 13);
     });
 });
