4a208a8dc2fa689784e5193ad9e9e2cb49eb591b
idmacleod <idmacleod@gmail.com>
2020-07-29 16:04:01 +0100

    Taking player names from HTML form and creating a new game

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 const Game = require('./models/game');
 const Player = require('./models/player');
 
-console.log('App Loaded', document);
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', startGame);
+});
+
+const startGame = function (event) {
+    event.preventDefault();
+    const player1 = new Player(event.target.p1.value);
+    const player2 = new Player(event.target.p2.value);
+    const game = new Game(player1, player2);
+    console.dir(game);
+}
